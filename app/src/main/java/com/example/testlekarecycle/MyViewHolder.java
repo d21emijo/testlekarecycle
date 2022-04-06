7b9dedc7988104946b243c9550999e466d05c27f
d21emijo <d21emijo@student.his.se>
2022-04-06 14:51:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/testlekarecycle/MyViewHolder.java
@@ -0,0 +1,19 @@
+package com.example.testlekarecycle;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewHolder extends RecyclerView.ViewHolder {
+public TextView name;
+
+
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        name = itemView.findViewById(R.id.name);
+
+    }
+}
